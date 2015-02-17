359318f13563d3ded3811c02608773deaf961e68
tiffaniechia <tiffaniekyrachia@gmail.com>
2015-02-17 13:57:06 +0000

    completed Deque class

--- Deque.java
+++ Deque.java
@@ -1,4 +1,5 @@
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class Deque<Item> implements Iterable<Item> {
     public Node first;
@@ -18,6 +19,14 @@ public class Deque<Item> implements Iterable<Item> {
             this.next = node;
         }
         
+        public Node getPrevious() {
+            return previous;    
+        }
+        
+        public void setPrevious(Node node) {
+            this.previous = node;
+        }
+        
         public void setItem(Item newItem) {
            this.item = newItem;
         }
@@ -25,9 +34,7 @@ public class Deque<Item> implements Iterable<Item> {
         public Item getItem() {
            return this.item;
         }
-        
-        
-        
+               
     }
     
     public Deque() {
@@ -38,24 +45,103 @@ public class Deque<Item> implements Iterable<Item> {
    
     public boolean isEmpty() {
         return count == 0;    
-    }; 
+    } 
     
     public int size() {
         return this.count;    
-    };
+    }
     
     public void addFirst(Item item) {
+        if (item == null) {
+            throw new NullPointerException();
+        }
         Node oldFirst = first;
         first = new Node();
         first.setItem(item);
-        first.setNext(oldFirst);
+        if(isEmpty()) {
+            last = first;
+            first.setNext(null);
+        } else {
+            first.setNext(oldFirst);
+            oldFirst.setPrevious(first);
+        }
+        
+        first.setPrevious(null);
         count++;
     }
     
-//    public void addLast(Item item)           
-//   public Item removeFirst()                
-//   public Item removeLast()                 
-     public Iterator<Item> iterator(){ 
-         return null;
-     }         
+    public void addLast(Item item) {
+        if (item == null) {
+            throw new NullPointerException();
+        }
+        Node oldLast = last;
+        last = new Node();
+        last.setItem(item);
+        if(isEmpty()) {
+            first = last;
+            last.setPrevious(null);
+        } else {
+            oldLast.setNext(last);
+            last.setPrevious(oldLast);
+        }
+        last.setNext(null);
+        count++;
+    }           
+    
+    public Item removeFirst() {
+       if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        Item result = first.getItem();
+        if(count == 1) {
+            last = null;
+            first = null;
+        } else {
+            first = first.getNext();
+            first.setPrevious(null);
+        }
+        count--;  
+        return result;
+    }                
+  
+    public Item removeLast() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        Item result = last.getItem();
+        if(count == 1) {
+            last = null;
+            first = null;
+        } else {
+            last = last.getPrevious();
+            last.setNext(null); 
+        }
+        count--;
+        return result;
+    }     
+    
+    public Iterator<Item> iterator(){ 
+        return new DequeIterator();
+     }
+     
+    private class DequeIterator implements Iterator<Item> { 
+        private Node current = first;
+         
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+         
+        public boolean hasNext() {
+            return current != null;
+        }
+         
+        public Item next() {
+            Item item = current.getItem();
+            if (!hasNext()) { 
+                throw new NoSuchElementException();
+            }   
+            current = current.getNext();
+            return item;
+        }
+    }
 }
--- DequeTest.java
+++ DequeTest.java
@@ -4,10 +4,11 @@ import static org.junit.Assert.*;
 
 public class DequeTest {
     Deque<String> deque;
+    Deque<String> addedDeque;
     
     @Before
     public void setUp() {
-        deque= new Deque<String>();
+        deque = new Deque<String>();
     }
     
     @Test
@@ -20,12 +21,69 @@ public class DequeTest {
     }
     
     @Test
-    public void shouldCreateNewNodeWithNewNextWhenAddingNode() {
-        String newNode = "newNode";
-        deque.addFirst(newNode);
+    public void shouldCreateNewNodeWithSettingsWhenAddedToFront() {
+        String newNodeFront = "newNode";
+        deque.addFirst(newNodeFront);
         assertEquals(1,deque.size());
         assertEquals(null,deque.first.getNext());
-        assertEquals(newNode,deque.first.getItem());
+        assertEquals(newNodeFront,deque.first.getItem());
+        assertEquals(newNodeFront,deque.last.getItem());
+        assertEquals(null,deque.first.getPrevious());
     }
     
+    @Test
+    public void shouldCreateNewNodeWithSettingsWhenAddedToback() {
+        String newNodeBack = "newNode";
+        deque.addLast(newNodeBack);
+        assertEquals(1,deque.size());
+        assertEquals(null,deque.first.getNext());
+        assertEquals(newNodeBack,deque.last.getItem());
+        assertEquals(newNodeBack,deque.first.getItem());
+        assertEquals(null,deque.first.getPrevious());
+    }
+    
+    @Test 
+    public void shouldKnowItsItsSequenceAndItsNeighbours() {
+        String frontNode = "front";
+        String backNode = "back";
+        deque.addLast(frontNode);
+        deque.addLast(backNode);
+        
+        String firstItem = deque.first.getItem();
+        String firstNext = deque.first.getNext().getItem();
+        String lastItem = deque.last.getItem();
+        String lastPrevious = deque.last.getPrevious().getItem();
+       
+        assertEquals(2,deque.size());
+       
+        assertNull(deque.first.getPrevious());
+        assertEquals(backNode,firstNext);
+        assertEquals(frontNode,firstItem);
+        
+        assertEquals(backNode,lastItem);
+        assertEquals(frontNode,lastPrevious);
+        assertNull(deque.last.getNext());
+    }
+    
+    @Test
+    public void shouldRemoveAppropriateLinksWhenRemoved() {
+        String frontNode = "front";
+        String backNode = "back";
+        deque.addLast(frontNode);
+        deque.addLast(backNode);
+        deque.removeLast();
+
+        assertEquals(1,deque.size());   
+        assertNull(deque.first.getNext());       
+    }
+    
+    @Test
+    public void removeCleanlyIfNoPrevOrNext() {
+        String frontNode = "front";
+        deque.addLast(frontNode);
+        deque.removeFirst();
+        
+        assertEquals(0,deque.size());
+    }
+        
 }
